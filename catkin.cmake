47cc0fa0e43841d65e644463c52de37e986229ba
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-08-31 09:17:26 +0000

    change hrpsys to compile with  INSTALL_DIR=

--- catkin.cmake
+++ catkin.cmake
@@ -12,33 +12,61 @@ set(OPENRTM_DIR ${openrtm_aist_PREFIX}/lib/openrtm_aist)
 set(ENV{PKG_CONFIG_PATH} $ENV{PKG_CONFIG_PATH}:${CATKIN_DEVEL_PREFIX}/lib/pkgconfig)
 execute_process(
   COMMAND sh -c "test -e ${CATKIN_DEVEL_PREFIX}/share/hrpsys/ || rm -f ${PROJECT_SOURCE_DIR}/installed ${PROJECT_SOURCE_DIR}/build/hrpsys-base/CMakeCache.txt"
-  COMMAND cmake -E chdir ${PROJECT_SOURCE_DIR} make -f Makefile.hrpsys-base OPENRTM_DIR=${OPENRTM_DIR} installed
+  COMMAND cmake -E chdir ${PROJECT_SOURCE_DIR} make -f Makefile.hrpsys-base OPENRTM_DIR=${OPENRTM_DIR} INSTALL_DIR=${CATKIN_DEVEL_PREFIX} installed
                 RESULT_VARIABLE _make_failed)
 if (_make_failed)
   message(FATAL_ERROR "Compile hrpsys failed")
 endif(_make_failed)
+
+# binary files intentionally goes to ${CATKIN_PACKAGE_BIN_DESTINATION}/lib
 execute_process(
-  COMMAND sh -c "test -e ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME}/bin || (mkdir -p ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME}; mv ${CATKIN_DEVEL_PREFIX}/bin/* ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME}/)"
+  COMMAND sh -c "test -e ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME} || (mkdir -p ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME}; mv ${CATKIN_DEVEL_PREFIX}/bin/* ${CATKIN_DEVEL_PREFIX}/lib/${PROJECT_NAME}/)"
+  RESULT_VARIABLE _make_failed
   OUTPUT_VARIABLE _copy_bin)
-message("${_copy_bin}")
+message("copy binary files ${_copy_bin}")
+if (_make_failed)
+  message(FATAL_ERROR "Copy hrpsys/bin failed: ${_make_failed}")
+endif(_make_failed)
+
+# shared files intentionally goes to ${CATKIN_PACKAGE_SHARE_DESTINATION}
+execute_process(
+  COMMAND sh -c "test -e ${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME}/share/${PROJECT_NAME} || (mkdir -p ${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME}/share/${PROJECT_NAME}; mv ${CATKIN_DEVEL_PREFIX}/share/hrpsys/idl ${CATKIN_DEVEL_PREFIX}/share/hrpsys/samples ${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME}/share/${PROJECT_NAME})"
+  RESULT_VARIABLE _make_failed
+  OUTPUT_VARIABLE _copy_share)
+message("copy shared files ${_copy_share}")
+if (_make_failed)
+  message(FATAL_ERROR "Copy hrpsys/share failed: ${_make_failed}")
+endif(_make_failed)
+
+# plugin lib files intentionally goes to ${CATKIN_PACKAGE_SHARE_DESTINATION}
+execute_process(
+  COMMAND cmake -E chdir ${PROJECT_SOURCE_DIR}/build/hrpsys-base/rtc cmake -DCMAKE_INSTALL_PREFIX=${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME} -P cmake_install.cmake
+  COMMAND cmake -E remove_directory ${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME}/buhbin
+  COMMAND sh -c "(cd ${CATKIN_DEVEL_PREFIX}/share/${PROJECT_NAME}/lib; find -iname \"*.so\" -exec rm ${CATKIN_DEVEL_PREFIX}/lib/{} \;)"
+  RESULT_VARIABLE _make_failed
+  OUTPUT_VARIABLE _copy_lib)
+message("copy plugin library files ${_copy_lib}")
+if (_make_failed)
+  message(FATAL_ERROR "Copy hrpsys plugin libraries failed: ${_make_failed}")
+endif(_make_failed)
 
 
 catkin_package(
     DEPENDS jython libxml2 sdl opencv2 libqhull libglew-dev libirrlicht-dev boost doxygen openhrp3
     CATKIN-DEPENDS
-    INCLUDE_DIRS include
+    INCLUDE_DIRS ${CATKIN_DEVEL_PREFIX}/include
     SKIP_CMAKE_CONFIG_GENERATION
     SKIP_PKG_CONFIG_GENERATION
     # LIBRARIES # TODO
 )
 
 
-install(DIRECTORY bin/
+install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/bin/
   DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
   USE_SOURCE_PERMISSIONS  # set executable
 )
 
-install(DIRECTORY lib/
+install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/lib/
   DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
   FILES_MATCHING
   PATTERN "lib*.so"
@@ -50,17 +78,17 @@ install(DIRECTORY lib/
 #  "execute_process(COMMAND \"cp lib/* ${CATKIN_PACKAGE_SHARE_DESTINATION}/lib\")"
 #)
 
-install(DIRECTORY lib/
+install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/lib/
   DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}/lib
   PATTERN "lib*.so" EXCLUDE
   PATTERN "*.py" EXCLUDE
   PATTERN "*.pyc" EXCLUDE
   PATTERN "hrpsys-base.pc" EXCLUDE
 )
-install(DIRECTORY include
+install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/include
   DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
 )
-install(DIRECTORY share/hrpsys/
+install(DIRECTORY ${CATKIN_DEVEL_PREFIX}/share/hrpsys/
   DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
 )
 
